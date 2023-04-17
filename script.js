37815aba87f61f7aae173c7e186718618cce1a84
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-17 15:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 	for(let color in sampleObject){
-		if{key === color;
+		if(key === color)
 		return true;
 		  }
 	}
